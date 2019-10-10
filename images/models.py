2dce1a4c38f2be508ee81510ae1c34f1c4bd7ed1
agnes <aggyreina@gmail.com>
2019-10-10 14:45:29 +0200

    creates class category

--- images/models.py
+++ images/models.py
@@ -7,3 +7,54 @@ class Image(models.Model):
     name = models.CharField(max_length =30)
     description = models.TextField()
     pic_image = models.ImageField(upload_to = 'images/', null=True)
+
+    @classmethod
+    def get_all_images(cls):
+        images = cls.objects.all()
+        return images
+
+    def delete_image(self):
+        self.delete()
+
+    def save_image(self):
+        self.save()
+
+    @classmethod
+    def update_image(cls,id,value):
+        cls.objects.filter(id = id).update(pic_image = pic_image)
+
+    @classmethod
+    def get_image_by_id(cls,id):
+        image = cls.objects.filter(id = id).all()
+        return image
+
+    @classmethod
+    def get_image_by_category(cls,category):
+        images = cls.objects.filter(image_picture = category)
+        return images
+    
+    @classmethod
+    def get_image_by_location(cls,location):
+        images = cls.objects.filter(image_location = location)
+        return images
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    categories = (("dogs","dogs"),("cats","cats")) 
+    category = models.CharField(max_length = 255, choices = categories)
+
+
+    def __str__(self):
+        return f"{self.category}"
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    @classmethod
+    def update_category(cls,id,new_category):
+        cls.objects.filter(id = id).update(category = new_category)
--- images/views.py
+++ images/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Image
 
 # Create your views here.
 
 def welcome(request):
-    return render(request, 'welcome.html')
+    images = Image.get_all_images()
+    return render(request, 'welcome.html', {"images":images})
+
+def search_image(request):
+
+    if 'image' in request.GET and request.GET['image']:
+        search_term = request.GET.get('image')
+        searched_image = Image.get_image_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html', {"searched_image":searched_image,"message":message})
