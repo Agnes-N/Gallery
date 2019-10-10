565fded4d5cc1f7363aaccca62951a2830370931
agnes <aggyreina@gmail.com>
2019-10-10 11:16:36 +0200

    creates Image model

--- images/models.py
+++ images/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
-class Image
+
+class Image(models.Model):
+    name = models.CharField(max_length =30)
+    description = models.TextField()
+    pic_image = models.ImageField(upload_to = 'images/', null=True)
